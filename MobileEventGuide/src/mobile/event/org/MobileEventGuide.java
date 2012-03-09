62106fcb2be6b012f82ee53ae68cee96912504ed
george dagkakis <gdagkakis@yahoo.com>
2012-03-09 18:37:44 +0200

    1st

---
+++ MobileEventGuide/src/mobile/event/org/MobileEventGuide.java
@@ -0,0 +1,13 @@
+package mobile.event.org;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobileEventGuide extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
