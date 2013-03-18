c09f89ce72c45a05dfbf066e5560c80a023723fa
Sebastien Robin <seb@nexedi.com>
2013-03-18 16:45:31 +0100

    setup dummy code for platform

---
+++ dream/platform/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask import request
+from crossdomain import crossdomain
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    app.logger.debug('welcome')
+    return "Welcome to DREAM Simulation"
+
+@app.route("/addModel")
+def addModel():
+  pass
+
+def main(*args):
+  app.run(debug=True)
+
+@app.route("/someTest", methods=["POST", "OPTIONS"])
+@crossdomain(origin='*')
+def someTest():
+  app.logger.debug('someTest')
+  app.logger.debug(request)
+  app.logger.debug(request.__dict__)
+  app.logger.debug("request headers content type: %r" % (request.headers['Content-Type'],))
+  app.logger.debug("request.json: %r" % (request.json,))
+  response =request.json
+  return jsonify(request.json)
+
+if __name__ == "__main__":
+  main()
