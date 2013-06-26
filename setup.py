bb8f90dfe2bc4189c9793992771397dc669a8992
Jérome Perrin <jerome@nexedi.com>
2013-06-26 11:34:38 +0200

    move simulation under dream
    This creates a dream.simulation namespace

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     entry_points=("""
     [console_scripts]
     dream_platform=dream.platform:main
-    dream_simulation=simulation.LineGenerationJSON:main
+    dream_simulation=dream.simulation.LineGenerationJSON:main
     """),
     include_package_data=True,
     zip_safe=False,
