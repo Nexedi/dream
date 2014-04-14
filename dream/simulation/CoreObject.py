fb344c23a12ef477e5607bb75c61ad110a244b03
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2014-04-14 18:06:14 +0100

    comments for debugging added, to be removed

--- dream/simulation/CoreObject.py
+++ dream/simulation/CoreObject.py
@@ -308,6 +308,10 @@ class CoreObject(Process):
                     return False
                 activeObject.receiver=activeObject.selectReceiver(possibleReceivers)
                 activeObject.receiver.giver=activeObject
+            #===================================================================
+            # TESTING
+#             print now(), self.id, 'signaling receiver', self.receiver.id
+            #===================================================================
             activeObject.receiver.isRequested.signal(activeObject)
             return True
         return False
@@ -346,6 +350,10 @@ class CoreObject(Process):
                     return False
                 activeObject.giver=activeObject.selectGiver(possibleGivers)
                 activeObject.giver.receiver=activeObject
+            #===================================================================
+            # TESTING
+#             print now(), self.id, 'signaling giver', self.giver.id
+            #===================================================================
             activeObject.giver.canDispose.signal(activeObject)
             return True
         return False
