f70ce67aa60a459ecf13bfa006da279e8892143f
Romain Courteaud <romain@nexedi.com>
2014-06-05 16:45:38 +0200

    Commit built version.

---
+++ dream/platform/static/dream/queue_stat_graph.js
@@ -0,0 +1,42 @@
+/*global console, rJS, RSVP, initDocumentPageMixin, jQuery */
+/*jslint nomen: true */
+(function(window, rJS, RSVP, initDocumentPageMixin, $) {
+    "use strict";
+    function queue_stat_widget(output_data) {
+        /* FIXME: does not support more than one replic.
+     * + see george email to integrate without the need of an EG
+     */
+        var series = [];
+        $.each(output_data.elementList, function(idx, el) {
+            if (el._class === "Dream.Queue") {
+                series.push({
+                    label: el.name || el.id,
+                    data: el.wip_stat_list
+                });
+            }
+        });
+        return series;
+    }
+    var gadget_klass = rJS(window);
+    initDocumentPageMixin(gadget_klass);
+    gadget_klass.ready(function(g) {
+        g.props = {};
+    }).ready(function(g) {
+        return g.getElement().push(function(element) {
+            g.props.element = element;
+        });
+    }).declareAcquiredMethod("aq_getAttachment", "jio_getAttachment").declareMethod("render", function(options) {
+        var jio_key = options.id, gadget = this;
+        gadget.props.jio_key = jio_key;
+        return gadget.aq_getAttachment({
+            _id: gadget.props.jio_key,
+            _attachment: "simulation.json"
+        }).push(function(simulation_json) {
+            gadget.props.series = queue_stat_widget(// XXX Hardcoded result
+            JSON.parse(simulation_json)[0].result);
+        });
+    }).declareMethod("startService", function() {
+        // XXX Manually calculate width and height when resizing
+        $.plot(this.props.element.querySelector(".graph_container"), this.props.series);
+    });
+})(window, rJS, RSVP, initDocumentPageMixin, jQuery);
