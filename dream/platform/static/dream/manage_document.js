6ac75ea91c2b407a47f99f6eaf06c1a3a6daa817
Romain Courteaud <romain@nexedi.com>
2014-06-12 16:11:16 +0200

    Update static version.

--- dream/platform/static/dream/manage_document.js
+++ dream/platform/static/dream/manage_document.js
@@ -1,5 +1,5 @@
-/*global console, rJS, RSVP, initDocumentPageMixin */
-(function(window, rJS, RSVP, initDocumentPageMixin) {
+/*global console, rJS, RSVP, initDocumentPageMixin, jQuery */
+(function(window, rJS, RSVP, initDocumentPageMixin, $) {
     "use strict";
     function datatouri(data, mime_type) {
         var result = "data:";
@@ -29,6 +29,70 @@
         }
         return new RSVP.Promise(resolver, canceller);
     }
+    function disableAllButtons(gadget) {
+        // Prevent double click
+        var i, button_list = gadget.props.element.getElementsByClassName("ui-btn");
+        for (i = 0; i < button_list.length; i += 1) {
+            button_list[i].disabled = true;
+        }
+    }
+    function waitForKnowledgeExtraction(gadget) {
+        return new RSVP.Queue().push(function() {
+            return promiseEventListener(gadget.props.element.getElementsByClassName("knowledge_form")[0], "submit", false);
+        }).push(function() {
+            disableAllButtons(gadget);
+            return gadget.aq_getAttachment({
+                _id: gadget.props.jio_key,
+                _attachment: "body.json"
+            });
+        }).push(function(body_json) {
+            $.mobile.loading("show");
+            // XXX Hardcoded relative URL
+            return gadget.aq_ajax({
+                url: "../../runKnowledgeExtraction",
+                type: "POST",
+                data: body_json,
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            });
+        }).push(undefined, function(error) {
+            // Always drop the loader
+            $.mobile.loading("hide");
+            throw error;
+        }).push(function(evt) {
+            $.mobile.loading("hide");
+            var json_data = JSON.parse(evt.target.responseText);
+            if (json_data.success !== true) {
+                throw new Error(json_data.error);
+            }
+            return gadget.aq_putAttachment({
+                _id: gadget.props.jio_key,
+                _attachment: "body.json",
+                _data: JSON.stringify(json_data.data, null, 2),
+                _mimetype: "application/json"
+            });
+        }).push(function() {
+            return gadget.whoWantToDisplayThisDocument(gadget.props.jio_key);
+        }).push(function(url) {
+            return gadget.pleaseRedirectMyHash(url);
+        });
+    }
+    function waitForDeletion(gadget) {
+        return new RSVP.Queue().push(function() {
+            return promiseEventListener(gadget.props.element.getElementsByClassName("delete_form")[0], "submit", false);
+        }).push(function() {
+            disableAllButtons(gadget);
+            // Delete jIO document
+            return gadget.aq_remove({
+                _id: gadget.props.jio_key
+            });
+        }).push(function(result) {
+            return gadget.whoWantToDisplayHome();
+        }).push(function(url) {
+            return gadget.pleaseRedirectMyHash(url);
+        });
+    }
     var gadget_klass = rJS(window);
     initDocumentPageMixin(gadget_klass);
     gadget_klass.ready(function(g) {
@@ -37,7 +101,7 @@
         return g.getElement().push(function(element) {
             g.props.element = element;
         });
-    }).declareAcquiredMethod("aq_remove", "jio_remove").declareAcquiredMethod("aq_getAttachment", "jio_getAttachment").declareAcquiredMethod("aq_get", "jio_get").declareAcquiredMethod("pleaseRedirectMyHash", "pleaseRedirectMyHash").declareAcquiredMethod("whoWantToDisplayHome", "whoWantToDisplayHome").declareMethod("render", function(options) {
+    }).declareAcquiredMethod("aq_remove", "jio_remove").declareAcquiredMethod("aq_getAttachment", "jio_getAttachment").declareAcquiredMethod("aq_putAttachment", "jio_putAttachment").declareAcquiredMethod("aq_get", "jio_get").declareAcquiredMethod("aq_ajax", "jio_ajax").declareAcquiredMethod("pleaseRedirectMyHash", "pleaseRedirectMyHash").declareAcquiredMethod("whoWantToDisplayThisDocument", "whoWantToDisplayThisDocument").declareAcquiredMethod("whoWantToDisplayHome", "whoWantToDisplayHome").declareMethod("render", function(options) {
         this.props.jio_key = options.id;
         var gadget = this;
         return new RSVP.Queue().push(function() {
@@ -53,20 +117,6 @@
             export_link.href = datatouri(result_list[1], "application/json");
         });
     }).declareMethod("startService", function() {
-        var gadget = this;
-        return new RSVP.Queue().push(function() {
-            return promiseEventListener(gadget.props.element.getElementsByClassName("delete_form")[0], "submit", false);
-        }).push(function() {
-            // Prevent double click
-            gadget.props.element.getElementsByClassName("ui-btn")[0].disabled = true;
-            // Delete jIO document
-            return gadget.aq_remove({
-                _id: gadget.props.jio_key
-            });
-        }).push(function(result) {
-            return gadget.whoWantToDisplayHome();
-        }).push(function(url) {
-            return gadget.pleaseRedirectMyHash(url);
-        });
+        return RSVP.all([ waitForDeletion(this), waitForKnowledgeExtraction(this) ]);
     });
-})(window, rJS, RSVP, initDocumentPageMixin);
+})(window, rJS, RSVP, initDocumentPageMixin, jQuery);
---
+++ dream/platform/static/list_field/listfield.js
@@ -0,0 +1,30 @@
+/*global window, rJS, console, RSVP, Handlebars */
+/*jslint nomen: true */
+(function(window, rJS, console, RSVP, Handlebars) {
+    "use strict";
+    /////////////////////////////////////////////////////////////////
+    // Handlebars
+    /////////////////////////////////////////////////////////////////
+    // Precompile the templates while loading the first gadget instance
+    var gadget_klass = rJS(window), option_source = gadget_klass.__template_element.getElementById("option-template").innerHTML, option_template = Handlebars.compile(option_source), selected_option_source = gadget_klass.__template_element.getElementById("option-template").innerHTML, selected_option_template = Handlebars.compile(selected_option_source);
+    gadget_klass.ready(function(g) {
+        return g.getElement().push(function(element) {
+            g.element = element;
+        });
+    }).declareMethod("render", function(options) {
+        var select = this.element.getElementsByTagName("select")[0], i, template, field_json = options.field_json, tmp = "";
+        select.setAttribute("name", field_json.key);
+        for (i = 0; i < field_json.items.length; i += 1) {
+            if (field_json.items[i][1] === field_json.default[0]) {
+                template = selected_option_template;
+            } else {
+                template = option_template;
+            }
+            tmp += template({
+                value: field_json.items[i][1],
+                text: field_json.items[i][0]
+            });
+        }
+        select.innerHTML += tmp;
+    });
+})(window, rJS, console, RSVP, Handlebars);
