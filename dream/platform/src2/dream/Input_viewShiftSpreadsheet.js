ae60f68f09d2b4ec01bf81e51cf0880365c2ebd8
Romain Courteaud <romain@nexedi.com>
2014-06-18 16:52:04 +0200

    Allow to save shift spreadsheet

--- dream/platform/src2/dream/Input_viewShiftSpreadsheet.js
+++ dream/platform/src2/dream/Input_viewShiftSpreadsheet.js
@@ -1,6 +1,53 @@
-/*global rJS, RSVP, initGadgetMixin */
-(function (window, rJS, RSVP, initGadgetMixin) {
+/*global rJS, RSVP, initGadgetMixin, loopEventListener */
+(function (window, rJS, RSVP, initGadgetMixin, loopEventListener) {
   "use strict";
+
+  function saveSpreadsheet(evt) {
+    var gadget = this,
+      editor_data,
+      editor_gadget;
+    return new RSVP.Queue()
+      .push(function () {
+        // Prevent double click
+        evt.target.getElementsByClassName("ui-btn")[0].disabled = true;
+        return gadget.getDeclaredGadget("tableeditor");
+      })
+      .push(function (tablegadget) {
+        editor_gadget = tablegadget;
+        return editor_gadget.getData();
+      })
+      .push(function (data) {
+        editor_data = data;
+        // Always get a fresh version, to prevent deleting spreadsheet & co
+        return gadget.aq_getAttachment({
+          "_id": gadget.props.jio_key,
+          "_attachment": "body.json"
+        });
+      })
+      .push(function (body) {
+        var data = JSON.parse(body);
+        data.shift_spreadsheet = JSON.parse(editor_data);
+        return gadget.aq_putAttachment({
+          "_id": gadget.props.jio_key,
+          "_attachment": "body.json",
+          "_data": JSON.stringify(data, null, 2),
+          "_mimetype": "application/json"
+        });
+      })
+      .push(function () {
+        evt.target.getElementsByClassName("ui-btn")[0].disabled = false;
+      });
+  }
+
+
+  function waitForSave(gadget) {
+    return loopEventListener(
+      gadget.props.element.getElementsByClassName("save_form")[0],
+      'submit',
+      false,
+      saveSpreadsheet.bind(gadget)
+    );
+  }
 
   var gadget_klass = rJS(window);
   initGadgetMixin(gadget_klass);
@@ -36,9 +83,13 @@
     })
 
     .declareMethod("startService", function () {
+      var gadget = this;
       return this.getDeclaredGadget("tableeditor")
         .push(function (tableeditor) {
-          return tableeditor.startService();
+          return RSVP.all([
+            tableeditor.startService(),
+            waitForSave(gadget)
+          ]);
         });
     });
-}(window, rJS, RSVP, initGadgetMixin));
+}(window, rJS, RSVP, initGadgetMixin, loopEventListener));
