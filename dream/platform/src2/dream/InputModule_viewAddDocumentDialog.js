734aabb109d349ce0ed4a4a1959574d20d0cc09d
Romain Courteaud <romain@nexedi.com>
2014-06-18 17:24:52 +0200

    Allow to create empty document.

--- dream/platform/src2/dream/InputModule_viewAddDocumentDialog.js
+++ dream/platform/src2/dream/InputModule_viewAddDocumentDialog.js
@@ -4,6 +4,106 @@
            initGadgetMixin) {
   "use strict";
 
+  function createDocument(gadget, name) {
+    var now = new Date();
+    // Create jIO document
+    return gadget.aq_post({
+      title: name,
+      type: "Dream",
+      format: "application/json",
+      modified: now.toUTCString(),
+      date: now.getFullYear() + "-" + (now.getMonth() + 1) + "-" +
+        now.getDate()
+    });
+  }
+
+  function waitForImport(gadget) {
+    var json_data,
+      name;
+
+    return new RSVP.Queue()
+      .push(function () {
+        return promiseEventListener(
+          gadget.props.element.getElementsByClassName("import_form")[0],
+          'submit',
+          false
+        );
+      })
+      .push(function (evt) {
+        // Prevent double click
+        evt.target
+           .getElementsByClassName("ui-btn")[0].disabled = true;
+
+        var file = evt.target.dream_import.files[0];
+        name = file.name;
+        return promiseReadAsText(file);
+      })
+      .push(function (json) {
+        json_data = json;
+        return createDocument(gadget, name);
+      })
+      .push(function (jio_document) {
+        // Add JSON as attachment
+        return gadget.aq_putAttachment({
+          "_id": jio_document.id,
+          "_attachment": "body.json",
+          "_data": json_data,
+          "_mimetype": "application/json"
+        });
+      });
+  }
+
+  function waitForNew(gadget) {
+    var json_data = {
+      nodes: {},
+      edges: {},
+      preference: {},
+      general: {},
+      wip_part_spreadsheet: [[
+        "Order ID",
+        "Due Date",
+        "Priority",
+        "Project Manager",
+        "Part",
+        "Part Type",
+        "Sequence",
+        "Processing Times",
+        "Prerequisites Parts"
+      ]],
+      shift_spreadsheet: [[
+        "Day",
+        "Machines", // XXX more generic name ?
+        "Start",
+        "End"
+      ]]
+    },
+      name = "FromScratch";
+
+    return new RSVP.Queue()
+      .push(function () {
+        return promiseEventListener(
+          gadget.props.element.getElementsByClassName("new_form")[0],
+          'submit',
+          false
+        );
+      })
+      .push(function (evt) {
+        // Prevent double click
+        evt.target
+           .getElementsByClassName("ui-btn")[0].disabled = true;
+        return createDocument(gadget, name);
+      })
+      .push(function (jio_document) {
+        // Add JSON as attachment
+        return gadget.aq_putAttachment({
+          "_id": jio_document.id,
+          "_attachment": "body.json",
+          "_data": JSON.stringify(json_data),
+          "_mimetype": "application/json"
+        });
+      });
+  }
+
   var gadget_klass = rJS(window);
   initGadgetMixin(gadget_klass);
   gadget_klass
@@ -20,48 +120,14 @@
     // declared methods
     /////////////////////////////////////////////////////////////////
     .declareMethod("startService", function () {
-      var gadget = this,
-        json_data,
-        name;
+      var gadget = this;
 
       return new RSVP.Queue()
         .push(function () {
-          return promiseEventListener(
-            gadget.props.element.getElementsByClassName("import_form")[0],
-            'submit',
-            false
-          );
-        })
-        .push(function (evt) {
-          // Prevent double click
-          gadget.props.element
-            .getElementsByClassName("ui-btn")[0].disabled = true;
-
-          var file = evt.target.dream_import.files[0];
-          name = file.name;
-          return promiseReadAsText(file);
-        })
-        .push(function (json) {
-          var now = new Date();
-          json_data = json;
-          // Create jIO document
-          return gadget.aq_post({
-            title: name,
-            type: "Dream",
-            format: "application/json",
-            modified: now.toUTCString(),
-            date: now.getFullYear() + "-" + (now.getMonth() + 1) + "-" +
-              now.getDate()
-          });
-        })
-        .push(function (jio_document) {
-          // Add JSON as attachment
-          return gadget.aq_putAttachment({
-            "_id": jio_document.id,
-            "_attachment": "body.json",
-            "_data": json_data,
-            "_mimetype": "application/json"
-          });
+          return RSVP.any([
+            waitForImport(gadget),
+            waitForNew(gadget)
+          ]);
         })
         .push(function (result) {
           return gadget.whoWantToDisplayThisDocument(result.id);
