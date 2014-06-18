54af62b4ac144b16baa8602d34924668cdf12416
Thibaut Frain <thibaut.frain@tiolive.com>
2014-06-18 14:15:21 +0200

    Optimize toolbox building (in memory)

--- dream/platform/src2/toolbox/toolbox.js
+++ dream/platform/src2/toolbox/toolbox.js
@@ -1,26 +1,44 @@
-/*global window, RSVP, $, rJS*/
-(function (window, RSVP, $, rJS) {
+/*global window, document, RSVP, rJS, Handlebars*/
+(function (window, document, RSVP, rJS, Handlebars) {
   "use strict";
-  rJS(window)
+  /*jslint nomen: true*/
+  var gadget_klass = rJS(window),
+    tool_template_source = gadget_klass.__template_element
+      .getElementById('tool-template').innerHTML,
+    tool_template = Handlebars.compile(tool_template_source);
+
+
+  gadget_klass
 
     .declareAcquiredMethod("getConfigurationDict", "getConfigurationDict")
 
-    .declareMethod("startService", function () {
+    .declareMethod("render", function () {
       var g = this;
-      return RSVP.all([
-        g.getElement(),
-        g.getConfigurationDict()
-      ])
-        .then(function (result) {
-          var render_element = $(result[0]).find("#tools-container");
-          $.each(result[1], function (key, val) {
-            var name = val.name || key.split('-')[1];
+      return new RSVP.Queue()
+        .push(function () {
+          return g.getConfigurationDict();
+        })
+        .push(function (config) {
+          g.tools_container = document.createElement('div');
+          g.tools_container.className = 'tools-container';
+          Object.keys(config).forEach(function (key) {
+            var name = config[key].name || key.split('-')[1];
             if (key !== 'Dream-Configuration') {
-              render_element.append('<div id="' + key + '" class="tool ' +
-                                    key + '">' +
-                                    name + "<ul/></div>");
+              g.tools_container.innerHTML += tool_template({
+                key: key,
+                name: name
+              });
             }
           });
         });
+    })
+
+    .declareMethod('startService', function () {
+      var g = this;
+      return g.getElement().then(function (element) {
+        element.querySelector('.tools')
+          .appendChild(g.tools_container);
+      });
     });
-}(window, RSVP, $, rJS));
+
+}(window, document, RSVP, rJS, Handlebars));
