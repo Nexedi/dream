a226846ac6fceef7537bfc3cf903c27885915f29
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2014-06-23 15:13:54 +0100

    printRoute methods added to Job and Operator, printRoute static method updated

--- dream/simulation/Operator.py
+++ dream/simulation/Operator.py
@@ -73,6 +73,8 @@ class Operator(ObjectResource):
         self.candidateStations=[]               # list of candidateStations of the stations (those stations that can receive an entity)
         
         self.schedule=[]                        # the working schedule of the resource, the objects the resource was occupied by and the corresponding times
+        # alias used by printRoute
+        self.alias=self.id
         
     @staticmethod
     def getSupportedSchedulingRules():
@@ -351,7 +353,7 @@ class Operator(ObjectResource):
             G.outputIndex+=1
         G.outputIndex+=1
 
-       # =======================================================================
+    # =======================================================================
     #                    outputs results to JSON File
     # =======================================================================
     def outputResultsJSON(self):
@@ -367,4 +369,50 @@ class Operator(ObjectResource):
             json['results']['working_ratio'] = getConfidenceIntervals(self.Working)
             json['results']['waiting_ratio'] = getConfidenceIntervals(self.Waiting)
         G.outputJSON['elementList'].append(json)
-        
+    
+    #===========================================================================
+    # print the route (the different stations the resource was occupied by)
+    #===========================================================================
+    def printRoute(self):
+        if self.schedule:
+            for record in self.schedule:
+                # find the station of this step
+                station=record[0]               # XXX should also hold a list with all the machines G.MachineList?
+                # find the column corresponding to the machine
+                from Globals import G
+                # XXX each machine should have at least 3 columns, 2 for the jobs and one for operators
+                if station in G.MachineList:
+                    machine_index=G.MachineList.index(station)
+                    # find the entrance time of this step
+                    entrance_time=record[1]         # the time entity entered station
+                    # find the row corresponding to the event and start placing the name of the Job in the G.cells_to_write
+                    entrance_time_index=G.events_list.index(entrance_time)
+                    # find the exit time of this step
+                    if len(record)==3:
+                        exit_time=record[2]             # the time the entity exited the station
+                        # find the row corresponding to the event and place the name of the Job in the cell, this is the last cell of this processing
+                        exit_time_index=G.events_list.index(exit_time)
+                    elif len(record)!=3:
+                        exit_time_index=len(G.events_list)
+                    # for the rows with indices entrance_time_index to exit_time_index print the id of the Job in the column of the machine_index
+                    for step in range(entrance_time_index,exit_time_index+1, 1):
+                        col_to_write=station.op_col_indx
+                        stepDone=False
+                        # check if the cell is already written, if yes, then modify it adding the new jobs but not overwrite it
+                        if not G.cells_to_write:
+                            G.cells_to_write.append({'row':step+1,
+                                                     'col':col_to_write,
+                                                     'worker':self.alias})
+                            G.routeTraceSheet.write(step+1, col_to_write, self.alias)
+                            continue
+                        for cell in G.cells_to_write:
+                            if cell['row']==step+1 and cell['col']==col_to_write:
+                                cell['worker']=cell['worker']+','+self.alias
+                                G.routeTraceSheet.write(cell['row'], cell['col'], cell['worker'])
+                                stepDone=True
+                                break
+                        if not stepDone:
+                            G.cells_to_write.append({'row':step+1,
+                                                     'col':col_to_write,
+                                                     'worker':self.alias})
+                            G.routeTraceSheet.write(step+1, col_to_write, self.alias)
