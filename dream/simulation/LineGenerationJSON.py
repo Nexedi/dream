f2020a97c7546f68eebba8355878b88c4ac7d195
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2014-09-28 01:18:28 +0100

    extra control for the case of BOM definition of components route

--- dream/simulation/LineGenerationJSON.py
+++ dream/simulation/LineGenerationJSON.py
@@ -402,6 +402,7 @@ def createWIP():
                 # if orders are provided separately (BOM) provide the parent order as argument  
                 if entityOrder:
                     entity=entityType(order=order,**inputDict)
+                    entity.orderInBOM=True
                 else:
                     entity=entityType(**inputDict)
                 G.EntityList.append(entity)
