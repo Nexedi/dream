b0f9b0c34a65d30ca281085256b01c6a0ce74676
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2014-12-15 19:10:38 +0000

    handsontable updated to succesfully restore previously stored data

--- dream/platform/src/handsontable/handsontable.js
+++ dream/platform/src/handsontable/handsontable.js
@@ -6,11 +6,20 @@
   rJS(window)
     .declareMethod('render', function (content, options) {
       var data = JSON.parse(content),
-        i, def  = [];
+        i, ctrlflag = true, def  = [];
+      // if the content is not an array of arrays then create one 
+      // from the content
       for (i = 0; i <= data.length-1; i += 1) {
-        def.push(data[i].name);
+        if (!(data[i].constructor === Array)) {
+          def.push(data[i].name);
+        } else {
+          // otherwise do not modify anything
+          def = data;
+          ctrlflag = false;
+          break;
+        }
       }
-      def = [def];
+      if (ctrlflag) { def = [def]; }
 
       return this.getElement()
         .push(function (element) {
