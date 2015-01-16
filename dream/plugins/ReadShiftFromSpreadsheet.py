7d44f9b3a070b9b03a02e23ba167ad9a00b2acfd
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2015-01-16 17:32:11 +0000

    minor bugs in shift and skills plugins corrected

--- dream/plugins/ReadShiftFromSpreadsheet.py
+++ dream/plugins/ReadShiftFromSpreadsheet.py
@@ -31,18 +31,19 @@ class ReadShiftFromSpreadsheet(plugin.InputPreparationPlugin):
           shift_by_station.setdefault(station, []).append(
             (start_time, stop_time) )
 
-    for line in data['input']['operator_shift_spreadsheet'][1:]:
-      if line[1]:
-        # Get the dates, and convert them to simulation clock time units.
-        # In this class, time unit is a minute (XXX it can be an option)
-        start_date = strptime("%s %s" % (line[0], line[2]), '%Y/%m/%d %H:%M')
-        start_time = (start_date - now).total_seconds() // 60
-        stop_date = strptime("%s %s" % (line[0], line[3]), '%Y/%m/%d %H:%M')
-        stop_time = (stop_date - now).total_seconds() // 60
-        for station in line[1].split(','):
-          station = station.strip()
-          shift_by_station.setdefault(station, []).append(
-            (start_time, stop_time) )
+    if data['input']['operator_shift_spreadsheet']:
+      for line in data['input']['operator_shift_spreadsheet'][1:]:
+        if line[1]:
+          # Get the dates, and convert them to simulation clock time units.
+          # In this class, time unit is a minute (XXX it can be an option)
+          start_date = strptime("%s %s" % (line[0], line[2]), '%Y/%m/%d %H:%M')
+          start_time = (start_date - now).total_seconds() // 60
+          stop_date = strptime("%s %s" % (line[0], line[3]), '%Y/%m/%d %H:%M')
+          stop_time = (stop_date - now).total_seconds() // 60
+          for station in line[1].split(','):
+            station = station.strip()
+            shift_by_station.setdefault(station, []).append(
+              (start_time, stop_time) )
 
     for node, node_data in data['graph']['node'].items():
       if node in shift_by_station:
