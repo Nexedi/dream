ebc77d0487ef101e9392e3d59ff96f7e2ee71462
Jérome Perrin <jerome@nexedi.com>
2015-01-26 15:07:19 +0100

    Debug JSON widget to see the whole json
    This will replace the previous debug widget that shows input & output
    wheras we only have one json now

---
+++ dream/platform/src/dream/Input_viewDebugJson.js
@@ -0,0 +1,33 @@
+/*global rJS, initGadgetMixin */
+(function (window, rJS, initGadgetMixin) {
+  "use strict";
+
+  var gadget_klass = rJS(window);
+  initGadgetMixin(gadget_klass);
+  gadget_klass
+    /////////////////////////////////////////////////////////////////
+    // Acquired methods
+    /////////////////////////////////////////////////////////////////
+    .declareAcquiredMethod("aq_getAttachment", "jio_getAttachment")
+
+    /////////////////////////////////////////////////////////////////
+    // declared methods
+    /////////////////////////////////////////////////////////////////
+    .declareMethod("render", function (options) {
+      var gadget = this;
+      this.props.jio_key = options.id;
+      this.props.result = options.result;
+
+      return gadget.aq_getAttachment({
+        "_id": gadget.props.jio_key,
+        "_attachment": "body.json"
+      })
+        .push(function (result_json) {
+          var document = JSON.parse(result_json);
+          gadget.props.element.querySelector(".json").textContent =
+            JSON.stringify(document, undefined, " ");
+        });
+
+    });
+
+}(window, rJS, initGadgetMixin));
