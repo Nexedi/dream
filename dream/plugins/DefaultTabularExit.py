e568c462a132f5063a772b6df5a538dc2556e06b
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-02-06 16:20:18 +0000

    tab to get real exit results. only deterministic for now

--- dream/plugins/DefaultTabularExit.py
+++ dream/plugins/DefaultTabularExit.py
@@ -9,17 +9,18 @@ import xlrd
 from dream.plugins import plugin
 
 class DefaultTabularExit(plugin.OutputPreparationPlugin):
-  """ Output the exit stats in a tab
-  """
+    """ Output the exit stats in a tab
+    """
 
-  def postprocess(self, data):
-    print 'DefaultTabularExit'  
-    print data['result']['result_list']
-#     data['result']['result_list'].append({
-#         "key": "tabular results", 
-#         "elementList": [], 
-#         'view_tabular_results':[['a','b'],[1,2]]}
-#     )
-    data['result']['result_list'][0]['exit_output'] = [['a','b'],[1,2]] 
-    print data['result']['result_list']
-    return data
+    def postprocess(self, data):
+        node=data['graph']['node']
+        data['result']['result_list'][0]['exit_output'] = [['Exit Id','Throughput', 'Takt Time', 'Lifespan']]
+        for record in data['result']['result_list'][0]['elementList']:
+            family=record.get('family',None)
+            if family=='Exit':
+                exitId=record['id']
+                throughput=record['results'].get('throughput','undefined')
+                taktTime=record['results'].get('takt_time','undefined')
+                lifespan=record['results'].get('lifespan','undefined')
+                data['result']['result_list'][0]['exit_output'].append([exitId,throughput,taktTime,lifespan]) 
+        return data
