97d48c06796e8888516597f3ff1f2cf7a836f7c6
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-02-13 12:06:33 +0000

    remove getEntity from ExitJobShop. It does not need to override it

--- dream/simulation/ExitJobShop.py
+++ dream/simulation/ExitJobShop.py
@@ -64,20 +64,3 @@ class ExitJobShop(Exit):
                 return True
         return False
     
-    #===========================================================================
-    # overides the default getEntity
-    #===========================================================================
-    def getEntity(self):
-        activeEntity = Exit.getEntity(self)           #run the default method
-        from Globals import G
-        # for all the entities in the EntityList
-        for entity in G.EntityList:
-            requiredParts=entity.getRequiredParts()
-            if requiredParts:
-                # if the activeEntity is in the requierdParts of the entity
-                if activeEntity in requiredParts:
-                    # if the entity that requires the activeEntity can proceed then signal the currentStation of the entity
-                    if entity.checkIfRequiredPartsReady() and entity.currentStation.expectedSignals['canDispose']:
-                        self.sendSignal(receiver=entity.currentStation, signal=entity.currentStation.canDispose)
-        return activeEntity
-        
