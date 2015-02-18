cd52f5aa73996d23c278a09e6dab1f4c7572dd80
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-02-18 12:12:58 +0000

    plugin added in Batch instance so that the attributes of the shift are set correctly

---
+++ dream/plugins/BatchesShiftAttributes.py
@@ -0,0 +1,28 @@
+from copy import copy
+import json
+import time
+import random
+import operator
+from datetime import datetime
+
+from dream.plugins import plugin
+
+class BatchesShiftAttributes(plugin.InputPreparationPlugin):
+  """ Input preparation 
+      set the shift attributes in the Batch instance
+  """
+
+  def preprocess(self, data):
+    """ set the shift attributes in the Batch instance
+    """
+    nodes=data['graph']['node']
+    for node_id, node in nodes.iteritems():
+        interruptions=node.get('interruptions',None)
+        if interruptions:
+            shift=interruptions.get('shift',None)
+            if shift:
+                print 11111
+                interruptions['shift']['thresholdTimeIsOnShift']=0
+                interruptions['shift']['receiveBeforeEndThreshold']=7
+                interruptions['shift']['endUnfinished']=1
+    return data
