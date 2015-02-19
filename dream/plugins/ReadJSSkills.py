e7092d86cdd444efd7bd571918d8da3a4e8597a0
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2015-02-19 15:32:50 +0000

    ReadJSSkills corrected

--- dream/plugins/ReadJSSkills.py
+++ dream/plugins/ReadJSSkills.py
@@ -5,7 +5,6 @@ import random
 import operator
 from datetime import datetime
 import copy
-import routeQuery
 from datetime import datetime
 
 from dream.plugins import plugin
@@ -18,24 +17,25 @@ class ReadJSSkills(plugin.InputPreparationPlugin):
   def preprocess(self, data):
     """ Read the operator and the tasks they can perform and adds them to them to the graph. 
     """
-    PBData=data['input'].get('operator_skill_spreadsheet', None)
+    PBData=data['input'].get('operator_skills_spreadsheet', None)
     node=data['graph']['node']
     if PBData:
-        PBData.pop(0)  # pop the column names
-        for PBitem in PBData:
-            PBId=PBitem[0]
-            PBId = PBId.split("-")[0]
-            # in case there is no id, do not process the element
-            if not PBId:
-                continue
-            skills=PBitem[1].replace(" ","").split(';')
-            node[PBId]={
-              "_class": "Dream.Operator",
-              "capacity": 1,
-              "name":PBId,
-              "skills":skills,
-              "ouputSchedule" : 1
-            }
+      PBData.pop(0)  # pop the column names
+      for PBitem in PBData:
+        PBId=PBitem[0]
+        if PBId:
+          PBId = PBId.split("-")[0]
+        # in case there is no id, do not process the element
+        if not PBId:
+          continue
+        skills=PBitem[1].replace(" ","").split(';')
+        node[PBId]={
+          "_class": "Dream.Operator",
+          "capacity": 1,
+          "name":PBId,
+          "skills":skills,
+          "ouputSchedule" : 1
+        }
         
     return data
 
