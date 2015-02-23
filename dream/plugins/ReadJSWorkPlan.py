480c206cd34bc0b767cfc9338ee4809235decfb7
Ioannis Papagiannopoulos <ioannis.papagiannopoulos@ul.ie>
2015-02-23 13:29:25 +0000

    completed entry used to identify the whereabouts of the entities through their routes

--- dream/plugins/ReadJSWorkPlan.py
+++ dream/plugins/ReadJSWorkPlan.py
@@ -31,7 +31,6 @@ class ReadJSWorkPlan(plugin.InputPreparationPlugin):
     """
     self.data=data
     WPdata = data["input"].get("workplan_spreadsheet",[])
-    orders = data["input"]["BOM"].get("orders",{})
     if WPdata:
       WPdata.pop(0)  # pop the column names
       for line in WPdata:
@@ -55,11 +54,11 @@ class ReadJSWorkPlan(plugin.InputPreparationPlugin):
           order["componentsList"] = components
         # update  the route of the component
         route = part.get("route", [])
-        task_id = line[-1]
+        task_id = line[-2]
         sequence = line[4]
-        processingTime = line[-4]
+        processingTime = line[-5]
         operator = line[5]
-        partsneeded = line[-3]
+        partsneeded = line[-4]
         # if there are requested parts then split them
         if partsneeded:
           partsneeded = partsneeded.replace(" ","").split(',')
@@ -67,6 +66,7 @@ class ReadJSWorkPlan(plugin.InputPreparationPlugin):
           partsneeded = [""]
         technology = line[3]
         quantity = line[6]
+        completed = line[-1]
         # append the current step to the route of the part
         route.append({
           "task_id": task_id,
@@ -75,9 +75,8 @@ class ReadJSWorkPlan(plugin.InputPreparationPlugin):
           "operator": operator,
           "partsneeded": partsneeded,
           "technology": technology,
-          "quantity": quantity
+          "quantity": quantity,
+          "completed": completed
         })
         part["route"] = route
-
-	
     return data
