5683366689fc78ed70284d99fda914029754b7ac
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-02-23 17:27:41 +0000

    method to create a dict to post process results added

--- dream/plugins/BatchesTabularQueues.py
+++ dream/plugins/BatchesTabularQueues.py
@@ -8,50 +8,48 @@ import xlrd
 
 from dream.plugins import plugin
 
-class DefaultTabularExit(plugin.OutputPreparationPlugin):
+class BatchesTabularQueues(plugin.OutputPreparationPlugin):
     """ Output the exit stats in a tab
     """
 
     def postprocess(self, data):
         numberOfReplications=int(data['general']['numberOfReplications'])
         confidenceLevel=float(data['general']['confidenceLevel'])
+        maxSimTime=float(data['general']['maxSimTime'])
         if numberOfReplications==1:
             # create the titles of the columns
-            data['result']['result_list'][0]['exit_output'] = [['Exit Id','Throughput', 'Takt Time', 'Lifespan']]
+            data['result']['result_list'][-1]['buffer_output'] = [['Buffer','Final Value','Average',
+                                                                  'Std Dev','Min','Max',]]
             # loop the results and search for elements that have 'Exit' as family
-            for record in data['result']['result_list'][0]['elementList']:
+            for record in data['result']['result_list'][-1]['elementList']:
                 family=record.get('family',None)
                 # when found, add a row with the results of the specific exit
-                if family=='Exit':
-                    exitId=record['id']
-                    throughput=record['results'].get('throughput','undefined')
-                    taktTime=record['results'].get('takt_time','undefined')
-                    lifespan=record['results'].get('lifespan','undefined')
-                    data['result']['result_list'][0]['exit_output'].append([exitId,throughput,taktTime,lifespan]) 
+                if family=='Buffer':
+                    bufferId=record['id']
+                    wip_stat_list=record['results']['wip_stat_list'][0]
+                    bufferLevels=[int(x[1]) for x in wip_stat_list]
+                    minLevel=min(bufferLevels)
+                    maxLevel=max(bufferLevels)
+                    self.createTimeListDict(wip_stat_list,maxSimTime)
         elif numberOfReplications>1:
             # create the titles of the columns
-            data['result']['result_list'][0]['exit_output'] = [['Exit Id','','Throughput','' , '','Takt Time','','', 'Lifespan',''],
-                                                               ['','LB','AVG','RB','LB','AVG','RB','LB','AVG','RB']]
-            for record in data['result']['result_list'][0]['elementList']:
-                family=record.get('family',None)
-                # when found, add a row with the results of the specific exit
-                if family=='Exit':
-                    exitId=record['id']
-                    throughput=self.getConfidenceInterval(record['results'].get('throughput','undefined'),confidenceLevel)
-                    taktTime=self.getConfidenceInterval(record['results'].get('takt_time','undefined'),confidenceLevel)
-                    lifespan=self.getConfidenceInterval(record['results'].get('lifespan','undefined'),confidenceLevel)                 
-                    data['result']['result_list'][0]['exit_output'].append([exitId,
-                                                                            throughput['lb'],throughput['avg'],throughput['ub'],
-                                                                            taktTime['lb'],taktTime['avg'],taktTime['ub'],
-                                                                            lifespan['lb'],lifespan['avg'],lifespan['ub']]) 
+            pass
         return data
-    
-    def getConfidenceInterval(self, value_list, confidenceLevel):
-        from dream.KnowledgeExtraction.ConfidenceIntervals import Intervals
-        from dream.KnowledgeExtraction.StatisticalMeasures import BasicStatisticalMeasures
-        BSM=BasicStatisticalMeasures()
-        lb, ub = Intervals().ConfidIntervals(value_list, confidenceLevel)
-        return {'lb': lb,
-                'ub': ub,
-                'avg': BSM.mean(value_list) 
-            }
+
+    # takes the time list that ManPy outputs and creates a dict so that it is easier to get avg etc
+    def createTimeListDict(self, timeList,maxSimTime):
+        timeListDict={}
+        i=0
+        for record in timeList:
+            time=record[0]
+            level=int(record[1])
+            try:
+                nextTime=timeList[i+1][0]
+            except IndexError:
+                nextTime=maxSimTime
+            i+=1
+            if not (level in timeListDict.keys()):
+                timeListDict[level]=0
+            timeListDict[level]+=nextTime-time
+        print timeListDict
+            
