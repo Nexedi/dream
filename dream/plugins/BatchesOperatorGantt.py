8448a389aa403c32c513b9c32f38a39cf88e4415
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-02-24 19:05:30 +0000

    remove test code

--- dream/plugins/BatchesOperatorGantt.py
+++ dream/plugins/BatchesOperatorGantt.py
@@ -36,11 +36,6 @@ class BatchesOperatorGantt(plugin.OutputPreparationPlugin, TimeSupportMixin):
             open=False)
         
             k=1
-            schedule=element['results']['schedule']
-            schedule.append({
-                 "entranceTime": 90.0, 
-                 "stationId": "M1"
-            })
             for record in schedule:
                 entranceTime=record['entranceTime']
                 try:
@@ -48,8 +43,8 @@ class BatchesOperatorGantt(plugin.OutputPreparationPlugin, TimeSupportMixin):
                 except IndexError:
                     exitTime=maxSimTime    
                 k+=1     
-                task_dict[operatorId+record['stationId']+str(k)] = dict(
-                    id=operatorId+record['stationId']+str(k),
+                task_dict[operatorId+record['stationId']] = dict(
+                    id=operatorId+record['stationId'],
                     parent=operatorId,
                     text=record['stationId'],
                     start_date=self.convertToRealWorldTime(
