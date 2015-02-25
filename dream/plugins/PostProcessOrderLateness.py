07ab8d2407c09e8de2569c65be82a274652f23fd
Jérome Perrin <jerome@nexedi.com>
2015-02-25 15:15:12 +0100

    PostProcessOrderLateness: read due date from the BOM

--- dream/plugins/PostProcessOrderLateness.py
+++ dream/plugins/PostProcessOrderLateness.py
@@ -11,22 +11,24 @@ class PostProcessOrderLateness(plugin.OutputPreparationPlugin, TimeSupportMixin)
       order_lateness_dict = result[self.configuration_dict["output_id"]] = {}
       for obj in result['elementList']:
         if obj.get('_class') == "Dream.OrderDesign": # XXX How to find orders ?
+          dueDate, = [order['dueDate'] for order in data['input']['BOM']['productionOrders'] if order['id'] == obj['id']]
           order_lateness_dict[obj["id"]] = {
-            "dueDate": self.convertToFormattedRealWorldTime(obj["results"]["completionTime"] - obj["results"].get("delay", 0)),
+            "dueDate": self.convertToFormattedRealWorldTime(dueDate),
             "delay": obj["results"].get("delay", 0), 
             "completionDate": self.convertToFormattedRealWorldTime(obj["results"]["completionTime"])
           }
         if obj.get('_class') == "Dream.CapacityProject": # XXX How to find orders ?
+          dueDate, = [order['dueDate'] for order in data['input']['BOM']['productionOrders'] if order['id'] == obj['id']]
           if obj["results"]["schedule"]:
             completionTime = obj["results"]["schedule"][-1]["exitTime"]
             order_lateness_dict[obj["id"]] = {
-              "dueDate": self.convertToFormattedRealWorldTime(obj["dueDate"]),
-              "delay": completionTime - obj["dueDate"],
+              "dueDate": self.convertToFormattedRealWorldTime(dueDate),
+              "delay": completionTime - dueDate,
               "completionDate": self.convertToFormattedRealWorldTime(completionTime)
             }
           else:
             order_lateness_dict[obj["id"]] = {
-              "dueDate": self.convertToFormattedRealWorldTime(obj["dueDate"]),
+              "dueDate": self.convertToFormattedRealWorldTime(dueDate),
               "delay": 1000,
               "completionDate": "Unfinished"
             }
