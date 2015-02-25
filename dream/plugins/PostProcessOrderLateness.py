e551ed329fa820584037b732177188a11bd7b6d4
Jérome Perrin <jerome@nexedi.com>
2015-02-25 11:01:41 +0100

    PostProcessOrderLateness: also support capacity projects

--- dream/plugins/PostProcessOrderLateness.py
+++ dream/plugins/PostProcessOrderLateness.py
@@ -7,14 +7,28 @@ class PostProcessOrderLateness(plugin.OutputPreparationPlugin, TimeSupportMixin)
 
   def postprocess(self, data):
     self.initializeTimeSupport(data)
-    
     for result in data['result']['result_list']:
+      order_lateness_dict = result[self.configuration_dict["output_id"]] = {}
       for obj in result['elementList']:
         if obj.get('_class') == "Dream.OrderDesign": # XXX How to find orders ?
-          result.setdefault(self.configuration_dict["output_id"], {})[obj["id"]] = {
+          order_lateness_dict[obj["id"]] = {
             "dueDate": self.convertToFormattedRealWorldTime(obj["results"]["completionTime"] - obj["results"].get("delay", 0)),
             "delay": obj["results"].get("delay", 0), 
             "completionDate": self.convertToFormattedRealWorldTime(obj["results"]["completionTime"])
           }
+        if obj.get('_class') == "Dream.CapacityProject": # XXX How to find orders ?
+          if obj["results"]["schedule"]:
+            completionTime = obj["results"]["schedule"][-1]["exitTime"]
+            order_lateness_dict[obj["id"]] = {
+              "dueDate": self.convertToFormattedRealWorldTime(obj["dueDate"]),
+              "delay": completionTime - obj["dueDate"],
+              "completionDate": self.convertToFormattedRealWorldTime(completionTime)
+            }
+          else:
+            order_lateness_dict[obj["id"]] = {
+              "dueDate": self.convertToFormattedRealWorldTime(obj["dueDate"]),
+              "delay": 1000,
+              "completionDate": "Unfinished"
+            }
 
     return data
