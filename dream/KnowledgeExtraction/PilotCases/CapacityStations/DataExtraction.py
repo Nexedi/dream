27a889def9442a633dc7d2c56db17d6114031f88
Panos Barlas <panagiotis.barlas@ul.ie>
2015-03-23 18:26:15 +0000

    Bug fixed in the DataExtraction script

--- dream/KnowledgeExtraction/PilotCases/CapacityStations/DataExtraction.py
+++ dream/KnowledgeExtraction/PilotCases/CapacityStations/DataExtraction.py
@@ -16,7 +16,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with DREAM.  If not, see <http://www.gnu.org/licenses/>.
 # ===========================================================================
-import pyodbc
 import json
 import ImportDatabase 
 
