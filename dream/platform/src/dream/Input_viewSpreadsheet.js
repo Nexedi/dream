Nexedi/dream

commit 24d7df816ccbc733bf5bc6f74e295749349d2470

GUI: fix initial data on spreadsheet, this was lost during merges

dream/platform/src/handsontable/handsontable.js
@@ -5,13 +5,28 @@
 
   rJS(window)
     .declareMethod('render', function (content, options) {
-      var data = JSON.parse(content);
-      
+      var data = JSON.parse(content),
+              i, ctrlflag = true, def  = [];
+     
+      // if the content is not an array of arrays then create one
+      // from the content
+      for (i = 0; i <= data.length-1; i += 1) {
+        if (!(data[i].constructor === Array)) {
+          def.push(data[i].name);
+        } else {
+          // otherwise do not modify anything
+          def = data;
+          ctrlflag = false;
+          break;
+        }
+      }
+      if (ctrlflag) { def = [def]; }
+
       return this.getElement()
         .push(function (element) {
           $(element).find('.table-container')
                     .handsontable($.extend({
-              data: data,
+              data: def,
               stretchH: 'all'
             }, options || {}));
         });

commit a362559bc2839528a15145aebf5a3b9ef11bbd75

Fix initialisation of spreadsheet, 24d7df816ccbc733b introduced a regression

dream/platform/src/dream/Input_viewSpreadsheet.js
@@ -90,6 +90,7 @@
         })
         .push(function (result_list) {
           var content,
+            i, ctrlflag = true, def  = [],
             result = JSON.parse(result_list[0]),
             handsontable_options = {
               minSpareRows: 1,
@@ -109,6 +110,21 @@
             content = result.input[gadget.props.name];
           } else {
             content = options.action_definition.configuration.columns;
+            // XXX this is for the case of the initial setup
+            // if the content is not an array of arrays then create one
+            // from the content
+            for (i = 0; i <= content.length-1; i += 1) {
+              if (!(content[i].constructor === Array)) {
+                def.push(content[i].name);
+              } else {
+                // otherwise do not modify anything
+                def = content;
+                ctrlflag = false;
+                break;
+              }
+            }
+            if (ctrlflag) { def = [def]; }
+            content = def;
           }
           $.extend(handsontable_options, options.action_definition.configuration.handsontable_options || {});
           return result_list[1].render(

dream/platform/src/handsontable/handsontable.js
@@ -5,28 +5,13 @@
 
   rJS(window)
     .declareMethod('render', function (content, options) {
-      var data = JSON.parse(content),
-              i, ctrlflag = true, def  = [];
-     
-      // if the content is not an array of arrays then create one
-      // from the content
-      for (i = 0; i <= data.length-1; i += 1) {
-        if (!(data[i].constructor === Array)) {
-          def.push(data[i].name);
-        } else {
-          // otherwise do not modify anything
-          def = data;
-          ctrlflag = false;
-          break;
-        }
-      }
-      if (ctrlflag) { def = [def]; }
+      var data = JSON.parse(content);
 
       return this.getElement()
         .push(function (element) {
           $(element).find('.table-container')
                     .handsontable($.extend({
-              data: def,
+              data: data,
               stretchH: 'all'
             }, options || {}));
         });