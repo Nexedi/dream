243dffe8bf13c499782abf9c88d098bddf8e3eb7
Panos Barlas <panagiotis.barlas@ul.ie>
2015-04-16 13:12:07 +0100

    Bug fixed in the script for the executabe

--- dream/KnowledgeExtraction/PilotCases/JobShop/JobShopInterface_exe.py
+++ dream/KnowledgeExtraction/PilotCases/JobShop/JobShopInterface_exe.py
@@ -6,7 +6,7 @@ Created on 17 Jun 2014
 from distutils.core import setup
 import py2exe
 
-setup(console=['C:\Eclipseworkspace\dreamgui\dream\dream\KnowledgeExtraction\PilotCases\CapacityStations\CapacityStationsInterface2.py'],
+setup(console=['C:\Eclipseworkspace\dreamgui\dream\dream\KnowledgeExtraction\PilotCases\JobShop\JobShopInterface2.py'],
       options={
                 "py2exe": {
                         "includes": ["decimal"]
