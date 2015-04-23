8210f8c970b6974eb69057cc6ab2289606b063ee
Jérome Perrin <jerome@nexedi.com>
2015-04-23 08:59:46 +0200

    Spreadsheet widget: set height to allow loading very large spreadsheets
    ( in a quite ad-hoc way )

--- dream/platform/src/handsontable/handsontable.js
+++ dream/platform/src/handsontable/handsontable.js
@@ -31,6 +31,18 @@
       return this.getElement()
         .push(function (element) {
           gadget_element = element;
+          
+          // XXX does not belong here. We need to have an height and width to the
+          // handsontable element so that it loads only what is displayed.
+          // XXX This is also very dirty because of assumptions for DREAM page layout (.nav_container and header)
+          // This is to prevent  Error: Security brake: Too much TRs. Please define height for your table, which will enforce scrollbars.
+          function updateSize() {
+            $(element).find('.table-container').height( 
+              ($(window).height() - $(".nav_container").height() - $("header").height()));
+          }
+          $(window).resize(updateSize);
+          updateSize();
+          
           $(element).find('.table-container').handsontable("render");
         })
         .push(function () {
