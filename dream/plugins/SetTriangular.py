560b36245255ac072b5103f3fe7d48a7c5bf1b1b
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-05-12 16:40:23 +0100

    plugin to take a deterministic model and set distributions to triangular added

---
+++ dream/plugins/SetTriangular.py
@@ -0,0 +1,33 @@
+from copy import copy
+import json
+import time
+import random
+import operator
+from datetime import datetime
+
+from dream.plugins import plugin
+
+# XXX HARDCODED
+MACHINE_TYPE_SET = set(["Dream.MachineJobShop", "Dream.MouldAssembly","Dream.Machine","Dream.BatchScrapMachine"])
+
+class SetTriangular(plugin.InputPreparationPlugin):
+    """ Input preparation
+        takes a model with deterministic processing time and sets it to triangular
+    """
+
+    def preprocess(self, data):
+        nodes=data['graph']['node']
+        for node_id,node in nodes.iteritems():
+            if node.get('_class',None)==self.configuration_dict['input_id']:
+                processingTime=node.get('processingTime',{})
+                distribution=processingTime.get("Fixed",{})
+                if distribution:
+                    mean=distribution['mean']
+                processingTime.pop('Fixed',None)
+                processingTime['Triangular']={
+                        "mean":mean,
+                        "min":0.8*mean,
+                        "max":1.2*mean
+                    }
+        return data
+    
