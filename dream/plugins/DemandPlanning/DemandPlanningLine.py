a54ea05d9da39e82c6cdd01721d29d41b0e4cbe0
Jérome Perrin <jerome@nexedi.com>
2015-05-27 12:33:14 +0200

    new plugin to postprocess demand planning bottleneck analysis by week

--- dream/plugins/DemandPlanning/DemandPlanningLine.py
+++ dream/plugins/DemandPlanning/DemandPlanningLine.py
@@ -62,3 +62,60 @@ class DemandPlanningLine(plugin.OutputPreparationPlugin, TimeSupportMixin):
             })
             
     return data
+
+
+
+class BottleNeckByWeek(plugin.OutputPreparationPlugin, TimeSupportMixin):
+  """ Output the queue statistics in a format compatible with Output_viewGraph, for the second widget by week.
+  """
+
+  def postprocess(self, data):
+ 
+    from dream.simulation.applications.DemandPlanning.Globals import G
+    
+    result = data['result']['result_list'][-1]
+    bottleNeckUtilizationDict = result['bottleneck_utilization_by_week'] = {}
+
+    by_week = {}
+    # change {bottleneck: {week: data }} in {week: {bottleneck: data}}
+    for bottleneck, bottleNeckUtilization in G.Utilisation.iteritems():
+        for record_id, record in bottleNeckUtilization.iteritems():
+            by_week.setdefault(record_id, {})[bottleneck] = record
+
+    for week, bottleneckData in by_week.items():
+      series = []
+      ticks = [list(enumerate(bottleneckData.keys()))]
+      
+      options = {
+        "xaxis": {
+          "minTickSize": 1,
+          "ticks": ticks
+        },
+        "series": {
+          "bars": {
+            "show": True,
+            "barWidth": 0.10,
+            "order": 1,
+            "align": "center"
+          },
+          "stack": False
+        }
+      }
+      
+      weekLabel = "%s %s" % (str(week)[:4], str(week)[4:])
+      bottleNeckUtilizationDict[weekLabel] = {
+        "series": series,
+        "options": options
+      }
+      
+      # create the 3 bars
+      for (utilizationType, utilizationLabel) in [
+                ( 'averageUtilization', 'Average Utilization' ),
+                ( 'minUtilization', 'Min Utilization' ),
+                ( 'maxUtilization', 'Max Utilization' ) ]:
+        series.append({
+            "label": utilizationLabel,
+            "data": list(enumerate([x[utilizationType] for x in bottleneckData.values()])),
+        })
+            
+    return data
