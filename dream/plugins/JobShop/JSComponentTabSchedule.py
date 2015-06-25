5d2d4dbe91986225bd85cefda8b054fbedb36855
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-06-25 16:59:12 +0100

    try statement added to save from bugs

--- dream/plugins/JobShop/JSComponentTabSchedule.py
+++ dream/plugins/JobShop/JSComponentTabSchedule.py
@@ -116,18 +116,22 @@ class JSComponentTabSchedule(plugin.OutputPreparationPlugin, TimeSupportMixin):
                                                                      self.convertToFormattedRealWorldTime(entranceTime),
                                                                      processingTime,
                                                                      operatorId])
-    # remove all the elements that have to do with buffers
-    tabElements=copy(result[self.configuration_dict['output_id']])
-    tabElements.pop(0)
-
-    for row in tabElements:
-        stationId=row[4]
-        stationClass=data['graph']['node'][stationId]['_class']
-        if stationClass in ['Dream.MouldAssemblyBuffer','Dream.ConditionalBuffer']:
-            result[self.configuration_dict['output_id']].remove(row)
     
-    # sort the elements according to Entrance Time        
-    firstRow=result[self.configuration_dict['output_id']].pop(0) 
-    result[self.configuration_dict['output_id']].sort(key=lambda x: x[5])
-    result[self.configuration_dict['output_id']].insert(0,firstRow)
+    try:
+        # remove all the elements that have to do with buffers
+        tabElements=copy(result[self.configuration_dict['output_id']])
+        tabElements.pop(0)
+    
+        for row in tabElements:
+            stationId=row[4]
+            stationClass=data['graph']['node'][stationId]['_class']
+            if stationClass in ['Dream.MouldAssemblyBuffer','Dream.ConditionalBuffer','Dream.OrderDecomposition']:
+                result[self.configuration_dict['output_id']].remove(row)
+        
+        # sort the elements according to Entrance Time        
+        firstRow=result[self.configuration_dict['output_id']].pop(0) 
+        result[self.configuration_dict['output_id']].sort(key=lambda x: x[5])
+        result[self.configuration_dict['output_id']].insert(0,firstRow)
+    except:
+        pass
     return data
