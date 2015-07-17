e3c6260988909c5a3322255264bd9bba80e4da3b
Jérome Perrin <jerome@nexedi.com>
2015-07-17 18:40:52 +0200

    update static version

---
+++ dream/platform/static/daff/daff.js
@@ -0,0 +1,17 @@
+/*global jQuery, rJS, window, JSON, daff */
+(function(window, $, rJS, JSON, daff) {
+    "use strict";
+    rJS(window).declareMethod("render", function(content) {
+        var flags, highlighter, diff2html, table_diff, table_diff_html, data_diff = [], data = JSON.parse(content), table1 = new daff.TableView(data[0]), table2 = new daff.TableView(data[1]), alignment = daff.compareTables(table1, table2).align();
+        table_diff = new daff.TableView(data_diff);
+        flags = new daff.CompareFlags();
+        highlighter = new daff.TableDiff(alignment, flags);
+        highlighter.hilite(table_diff);
+        diff2html = new daff.DiffRender();
+        diff2html.render(table_diff);
+        table_diff_html = diff2html.html();
+        return this.getElement().push(function(element) {
+            $(element).html(table_diff_html);
+        });
+    });
+})(window, jQuery, rJS, JSON, daff);
---
+++ dream/platform/static/dream/Output_viewSpreadsheetComparison.js
@@ -0,0 +1,49 @@
+/*global rJS, RSVP, initGadgetMixin, promiseEventListener, $, setTimeout */
+(function(window, rJS, RSVP, initGadgetMixin, promiseEventListener, $) {
+    "use strict";
+    var gadget_klass = rJS(window);
+    initGadgetMixin(gadget_klass);
+    gadget_klass.declareAcquiredMethod("aq_getAttachment", "jio_getAttachment").declareAcquiredMethod("aq_putAttachment", "jio_putAttachment").declareMethod("render", function(options) {
+        var jio_key = options.id, gadget = this;
+        gadget.props.jio_key = jio_key;
+        return new RSVP.Queue().push(function() {
+            return RSVP.all([ gadget.aq_getAttachment({
+                _id: jio_key,
+                _attachment: "body.json"
+            }), gadget.getDeclaredGadget("tablediff") ]);
+        }).push(function(result_list) {
+            var reference_spreadsheet = JSON.parse(result_list[0]).input.reference_spreadsheet || [], new_spreadsheet = JSON.parse(result_list[0]).result.result_list[options.result][options.action_definition.configuration.output_id];
+            gadget.props.new_spreadsheet = new_spreadsheet;
+            return result_list[1].render(JSON.stringify([ reference_spreadsheet, new_spreadsheet ]));
+        });
+    }).declareMethod("startService", function() {
+        var gadget = this;
+        return new RSVP.Queue().push(function() {
+            return promiseEventListener(gadget.props.element.querySelector(".set_reference_schedule_button"), "click", false);
+        }).push(function() {
+            return gadget.aq_getAttachment({
+                _id: gadget.props.jio_key,
+                _attachment: "body.json"
+            });
+        }).push(function(data) {
+            var object_data = JSON.parse(data);
+            // XXX option for that
+            object_data.input.reference_spreadsheet = gadget.props.new_spreadsheet;
+            return gadget.aq_putAttachment({
+                _id: gadget.props.jio_key,
+                _attachment: "body.json",
+                _data: JSON.stringify(object_data)
+            }).push(function() {
+                // XXX quick way to get a popup message
+                $.mobile.loading("show", {
+                    text: "Current schedule set as reference",
+                    textVisible: true,
+                    textonly: true
+                });
+                setTimeout(function() {
+                    $.mobile.loading("hide");
+                }, 1e3);
+            });
+        });
+    });
+})(window, rJS, RSVP, initGadgetMixin, promiseEventListener, $);
