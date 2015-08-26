767f4ae9b558eb5280b7ab6cff24057d3dcf1979
Georgios Dagkakis <georgios.dagkakis@ul.ie>
2015-08-26 13:37:02 +0100

    use strftime in plugin

--- dream/plugins/GetCurrentTime.py
+++ dream/plugins/GetCurrentTime.py
@@ -10,20 +10,6 @@ class GetCurrentTime(plugin.InputPreparationPlugin):
         currentTime=data['general'].get('currentDate',None)
         if not currentTime:
             currentTime=datetime.datetime.now()
-            year=currentTime.year
-            month=currentTime.month
-            day=currentTime.day
-            minute=currentTime.minute
-            hour=currentTime.hour
-            minute=currentTime.minute
-            second=currentTime.second
             dateFormat=data['general'].get('dateFormat','%Y/%m/%d %H:%M')
-            if dateFormat=='%Y/%m/%d %H:%M:%S':
-                currentTimeString=str(year)+'/'+str(month).zfill(2)+'/'+str(day).zfill(2)+' '+str(hour).zfill(2)+':'+str(minute).zfill(2)+':'+str(second).zfill(2)
-            elif dateFormat=='%Y/%m/%d %H:%M':
-                currentTimeString=str(year)+'/'+str(month).zfill(2)+'/'+str(day).zfill(2)+' '+str(hour).zfill(2)+':'+str(minute).zfill(2)
-            elif dateFormat=='%Y/%m/%d':
-                currentTimeString=str(year)+'/'+str(month).zfill(2)+'/'+str(day).zfill(2)
-            print currentTimeString
-            data['general']['currentDate']=currentTimeString      
+            data['general']['currentDate']=currentTime.strftime(dateFormat)               
         return data
