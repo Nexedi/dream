4f78a73e97a797e728591dfc019c1e4f68ec56b2
Jérome Perrin <jerome@nexedi.com>
2015-09-04 11:13:18 +0200

    update static version

--- dream/platform/static/dream/Input_viewSimulation.js
+++ dream/platform/static/dream/Input_viewSimulation.js
@@ -73,6 +73,11 @@
             });
         }).push(function() {
             return gadget.whoWantsToDisplayThisDocument(gadget.props.jio_key, "view_result");
+        }, function(reason) {
+            // Very likely, we exceeded the quota of the local storage.
+            // Let's handle it with a more informative message.
+            alert("Could not save simulation results.Please check the simulation parameters or try to delete some models.\n\nError was:" + reason);
+            return gadget.whoWantsToDisplayThisDocument(gadget.props.jio_key, "view");
         }).push(function(url) {
             return gadget.pleaseRedirectMyHash(url);
         });
