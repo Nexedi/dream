36d0c096de058655212d195d161d962c6a61839b
Jérome Perrin <jerome@nexedi.com>
2016-10-14 08:58:58 +0200

    update static version

--- dream/platform/static/dream/index.js
+++ dream/platform/static/dream/index.js
@@ -103,6 +103,17 @@
             return gadget.aq_pleasePublishMyState(forward_kw);
         });
     }
+    function getNextStepLink(gadget, portal_type, options) {
+        if (options.action === "view_machine_shift_spreadsheet") {
+            var forward_kw = {
+                action: "view_run_simulation",
+                id: options.id
+            };
+            return gadget.aq_pleasePublishMyState(forward_kw);
+        } else {
+            return false;
+        }
+    }
     function getTitle(gadget, portal_type, options) {
         var title;
         if (portal_type === "Input Module") {
@@ -330,7 +341,7 @@
                 return page_gadget.render(options);
             }
         }).push(function() {
-            return RSVP.all([ page_gadget.getElement(), calculateNavigationHTML(gadget, portal_type, options), gadget.aq_pleasePublishMyState(back_kw), getTitle(gadget, portal_type, options), getNextLink(gadget, portal_type, options) ]);
+            return RSVP.all([ page_gadget.getElement(), calculateNavigationHTML(gadget, portal_type, options), gadget.aq_pleasePublishMyState(back_kw), getTitle(gadget, portal_type, options), getNextLink(gadget, portal_type, options), getNextStepLink(gadget, portal_type, options) ]);
         }).push(function(result_list) {
             var nav_html = result_list[1], page_element = result_list[0];
             // Update title
@@ -358,6 +369,25 @@
             }
             element.appendChild(page_element);
             $(element).trigger("create");
+            return result_list[5];
+        }).push(function(next_step_link) {
+            var button = gadget.props.element.getElementsByClassName("next_step_link")[0];
+            $(button).hide();
+            var idle_timer;
+            function resetTimer() {
+                clearTimeout(idle_timer);
+                idle_timer = setTimeout(function() {
+                    $(button).show();
+                }, idle_seconds * 1);
+            }
+            if (next_step_link !== false) {
+                button.href = next_step_link;
+                var idle_seconds = 10;
+                $(document.body).on("keydown click", resetTimer);
+                resetTimer();
+            } else {
+                $(document.body).off("keydown click", resetTimer);
+            }
             // XXX RenderJS hack to start sub gadget services
             // Only work if this gadget has no parent.
             if (page_gadget.startService !== undefined) {
